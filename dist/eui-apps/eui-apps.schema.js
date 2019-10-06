1a2abb42c4b21dd76fea5501f4180a31392d143b
SARAL Ozan <ozan.saral@ext.ec.europa.eu>
2019-10-06 13:44:46 +0200

    lastUpdate is added

--- dist/eui-apps/eui-apps.schema.js
+++ dist/eui-apps/eui-apps.schema.js
@@ -5,16 +5,25 @@ exports.AppConfigurationSchema = new mongoose.Schema({
     features: Object,
     openIdConnect: Object,
     modules: Object,
+    lastUpdated: { type: Date, 'default': Date.now },
 });
 exports.EnvConfigSchema = new mongoose.Schema({
     envName: String,
     configuration: exports.AppConfigurationSchema,
     apiGatewayAppId: String,
+    lastUpdated: { type: Date, 'default': Date.now },
 });
 exports.EuiAppSchema = new mongoose.Schema({
     name: String,
-    description: String,
+    desc: String,
     envConfigs: [exports.EnvConfigSchema],
     roleId: [String],
+    lastUpdated: { type: Date, 'default': Date.now },
+});
+[exports.EnvConfigSchema, exports.EuiAppSchema, exports.AppConfigurationSchema].forEach((schema) => {
+    schema.pre('save', function (next) {
+        this.lastUpdated = Date.now();
+        next();
+    });
 });
 //# sourceMappingURL=eui-apps.schema.js.map
--- src/eui-apps/eui-apps.schema.ts
+++ src/eui-apps/eui-apps.schema.ts
@@ -4,12 +4,14 @@ export const AppConfigurationSchema = new mongoose.Schema({
     features: Object,
     openIdConnect: Object,
     modules: Object,
+    lastUpdated: { type: Date, 'default': Date.now },
 });
 
 export const EnvConfigSchema = new mongoose.Schema({
     envName: String,
     configuration: AppConfigurationSchema,
     apiGatewayAppId: String,
+    lastUpdated: { type: Date, 'default': Date.now },
 });
 
 export const EuiAppSchema = new mongoose.Schema({
@@ -17,4 +19,13 @@ export const EuiAppSchema = new mongoose.Schema({
     desc: String,
     envConfigs: [EnvConfigSchema],
     roleId: [String],
+    lastUpdated: { type: Date, 'default': Date.now },
 });
+[EnvConfigSchema, EuiAppSchema, AppConfigurationSchema].forEach((schema) => {
+    schema.pre('save', function (next) {
+        this.lastUpdated = Date.now();
+        next();
+    });
+});
+
+
